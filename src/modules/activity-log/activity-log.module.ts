59cc396a0a9c8f1093516d0a0fa244100652dd9b
sinanovicanes <sinanovicanes01@gmail.com>
2025-04-16 17:48:57 +0300

    refactor: make ActivityLogModule global so we can use interceptor anywhere

--- src/modules/activity-log/activity-log.module.ts
+++ src/modules/activity-log/activity-log.module.ts
@@ -1,11 +1,13 @@
-import { Module } from '@nestjs/common';
+import { Global, Module } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { ActivityLog } from './entities/activity-log.entity';
 import { ActivityLogService } from './activity-log.service';
 
 // TODO: Create controller to retrieve activity logs
+@Global()
 @Module({
   imports: [TypeOrmModule.forFeature([ActivityLog])],
   providers: [ActivityLogService],
+  exports: [ActivityLogService],
 })
 export class ActivityLogModule {}
